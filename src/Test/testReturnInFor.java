d188b3fd4d0acc60f5f34ef06df16dfc8b04a906
hxd <qufuhxd@163.com>
2017-05-09 15:17:39 +0800

    添加了一个类 15:16

---
+++ src/Test/testReturnInFor.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class testReturnInFor {
+   public static void main(String[] args) {
+	         for(int i=0;i<50;i++){
+	        	 
+	        	 if(i==10){
+	        		continue;
+	        	 }
+	        	 System.out.println(i);
+	         }
+}
+}
---
+++ src/Test/testScaner.java
@@ -0,0 +1,13 @@
+package Test;
+
+import java.util.Scanner;
+
+
+public class testScaner {
+   public static void main(String[] args) {
+	    String s="11";
+	    Scanner scanner=new Scanner(s);
+	    int a=scanner.nextInt();
+	    System.out.println(a);
+}
+}
