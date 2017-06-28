aeee8cd2ff9f284c2598392fd3e059082abf417d
hxd <qufuhxd@163.com>
2017-06-28 10:04:36 +0800

    testBranch添加

--- src/reset/reset.java
+++ src/reset/reset.java
@@ -8,5 +8,7 @@ package reset;
  * @date        2017年6月7日下午8:28:58
  */
 public class reset {
-
+        public static void main(String[] args) {
+			System.out.println("I am testBranch!!!");
+		}
 }
